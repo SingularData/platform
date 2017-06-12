52575e79024deeab38620053224aff71af8a5c56
haoliangyu <njkon@foxmail.com>
2017-06-11 22:38:23 -0400

    complete dockerization

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ WORKDIR /usr/src/app
 # Install app dependencies
 COPY package.json /usr/src/app/
 COPY package-lock.json /usr/src/app/
+COPY config /usr/src/app/
 RUN npm install
 
 # Bundle app source
@@ -14,6 +15,6 @@ COPY . /usr/src/app
 RUN npm run build
 
 # TODO: not use heroku next time
-ENV NODE_ENV=heroku
+ENV NODE_ENV=production
 EXPOSE 8082
 CMD [ "npm", "start" ]
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import * as express from 'express';
-import * as compression from 'compression';
 import * as config from 'config';
 import { resolve } from 'path';
 import { getLogger } from 'log4js';
@@ -10,12 +9,9 @@ import attachPortalAPI from './api/portal/index';
 
 const logger = getLogger('app');
 const app = express();
-
-// TODO: use something else, not Heroku
-const port = process.env.NODE_ENV === 'heroku' ? process.env.$PORT : config.get('port');
+const port = config.get('port');
 
 app.use(json())
-   .use(compression())
    .use(express.static('./public/www/'));
 
 attachDatasetAPI(app);
