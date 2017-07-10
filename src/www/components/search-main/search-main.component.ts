d2f30ee16b3d8d768134c625932425cdc0859046
haoliangyu <njkon@foxmail.com>
2017-07-09 21:09:54 -0400

    redo front page

--- src/www/components/search-main/search-main.component.ts
+++ src/www/components/search-main/search-main.component.ts
@@ -1,5 +1,5 @@
 import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'search-main',
@@ -9,48 +9,19 @@ import { Component } from '@angular/core';
     require('./search-main.component.less')
   ],
 })
-export default class SearchMainComponent {
-
-  private supportedPlatforms: Array<any>;
+export default class SearchMainComponent implements AfterViewInit {
 
   constructor(private router: Router) {
-    this.supportedPlatforms = [
-      {
-        name: 'CKAN',
-        logo: require('../../media/images/ckan-logo.png'),
-        url: 'http://ckan.org/'
-      },
-      {
-        name: 'DKAN',
-        logo: require('../../media/images/dkan-logo.png'),
-        url: 'http://www.nucivic.com/dkan/'
-      },
-      {
-        name: 'Socrata',
-        logo: require('../../media/images/socrata-logo.png'),
-        url: 'https://socrata.com/solutions/publica-open-data-cloud/'
-      },
-      {
-        name: 'Junar',
-        logo: require('../../media/images/junar-logo.jpg'),
-        url: 'http://junar.com/'
-      },
-      {
-        name: 'ArcGIS Open Data',
-        logo: require('../../media/images/arcgis-logo.png'),
-        url: 'http://opendata.arcgis.com/'
-      },
-      {
-        name: 'OpenDataSoft',
-        logo: require('../../media/images/opendatasoft-logo.png'),
-        url: 'https://www.opendatasoft.com/'
-      },
-      {
-        name: 'GeoNode',
-        logo: require('../../media/images/geonode-logo.png'),
-        url: 'http://geonode.org/'
-      }
-    ];
+  }
+
+  ngAfterViewInit() {
+    let placeholder = document.getElementsByClassName('mat-input-placeholder')[0] as HTMLElement;
+    placeholder.style.color = 'white';
+    placeholder.style.textAlign = 'center';
+
+    let underline = document.getElementsByClassName('mat-input-ripple')[0] as HTMLElement;
+    underline.style.visibility = 'visible';
+    underline.style.backgroundColor = 'white';
   }
 
   search(keywords: string): void {
