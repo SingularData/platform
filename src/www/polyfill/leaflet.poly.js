3dd58a27ed5d96e5b9af156a0850a9a588b45962
haoliangyu <njkon@foxmail.com>
2017-08-05 13:28:25 -0400

    clean up

--- src/www/polyfill/leaflet.poly.js
+++
@@ -1,7 +0,0 @@
-import * as L from 'leaflet';
-var DefaultIcon = L.icon({
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
-L.Marker.prototype.options.icon = DefaultIcon;
-//# sourceMappingURL=leaflet.poly.js.map
