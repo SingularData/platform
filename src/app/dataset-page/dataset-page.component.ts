e3425a4f4d48bb2c7d87b934b6135f774a5da431
Haoliang Yu <haoliangyu@users.noreply.github.com>
2018-03-08 00:30:41 -0500

    F/add coverage map (#3)
    * add coverage map
    
    * fix a lint error
    
    * update prepush hook

--- src/app/dataset-page/dataset-page.component.ts
+++ src/app/dataset-page/dataset-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 import { DatasetService } from "../dataset.service";
+import * as L from "leaflet";
 
 @Component({
   selector: "app-dataset-page",
@@ -8,14 +9,19 @@ import { DatasetService } from "../dataset.service";
   styleUrls: ["./dataset-page.component.scss"]
 })
 export class DatasetPageComponent implements OnInit {
+  @ViewChild("datasetMap") mapContainer;
+
   status: string;
   dcat: any;
   original: any;
+  map: L.Map;
 
   constructor(
     private activatedRouter: ActivatedRoute,
     private dataset: DatasetService
-  ) {}
+  ) {
+    this.status = "load";
+  }
 
   ngOnInit() {
     this.activatedRouter.params
@@ -26,6 +32,41 @@ export class DatasetPageComponent implements OnInit {
         result => {
           this.dcat = result.result.dcat;
           this.original = JSON.stringify(result.result.original, null, 2);
+
+          if (this.dcat.spatial) {
+            setTimeout(() => {
+              this.map = L.map(this.mapContainer.nativeElement, {
+                center: L.latLng(0, 0),
+                zoom: 2,
+                minZoom: 2,
+                preferCanvas: true,
+                worldCopyJump: true
+              });
+
+              L.tileLayer(
+                "http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+                {
+                  attribution:
+                    `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>` +
+                    `, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>`
+                }
+              ).addTo(this.map);
+
+              L.control.scale().addTo(this.map);
+
+              const region = L.geoJSON(this.dcat.spatial, {
+                style: () => {
+                  return {
+                    color: "#3F51B5",
+                    fillColor: "#3F51B5"
+                  };
+                }
+              }).addTo(this.map);
+
+              this.map.fitBounds(region.getBounds());
+            }, 500);
+          }
+
           this.status = "success";
         },
         error => {
